d19d9c8a8361729a323e807fbac6c855c4accd52
lunatiq-san <lunatiq86@gmail.com>
2022-01-03 18:28:35 +0200

    05-enums

---
+++ src/05-enums.ts
@@ -0,0 +1,16 @@
+/*
+ * Enum (enumeration)
+ *  - Enumeration pizza size
+ *  - Default values
+ *  - Custom values
+ */
+
+enum PizzaSize {
+  Small = "s",
+  Medium = "m",
+  Large = "l",
+}
+
+console.log(PizzaSize.Small);
+
+export {};
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 // import "./01-primitives";
 // import "./02-arrays";
 // import "./03-types";
-import "./04-interfaces";
+// import "./04-interfaces";
+import "./05-enums";
