6c7585a88643e0c9432b9c4027e7ad2fdf26e9bd
lunatiq-san <lunatiq86@gmail.com>
2022-01-03 13:17:04 +0200

    feature/01-primitives

---
+++ src/01-primitives.ts
@@ -0,0 +1,26 @@
+/*
+ * Primitive types
+ *  - Type declaration: number, string, boolean, null, undefined, any and object
+ *  - Errors:
+ *    - assigning a value of another type
+ *    - value override
+ *  - [type inference](https://www.typescriptlang.org/docs/handbook/type-inference.html)
+ *  - Multiple types with type composition
+ */
+
+let age: number = 5;
+const name: string = "mango";
+
+// Bad practiсe
+let value: any = 5;
+value = "some text";
+
+// Don't use
+let user: object = {};
+user = {};
+
+let id: string | number | boolean = 5;
+id = "text";
+id = true;
+
+export {};
