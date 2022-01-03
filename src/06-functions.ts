65b41a419c102ca8fcbb4d7c4d8a888a608de07e
lunatiq-san <lunatiq86@gmail.com>
2022-01-03 21:09:26 +0200

    Some changes

---
+++ src/06-functions.ts
@@ -0,0 +1,20 @@
+/*
+ * Functions
+ *  - Typing parameters and return value
+ *  - Optional parameters
+ *  - rest-parameters
+ *  - Function with no explicit return: type void
+ *  - Function signature typing
+ *    - through type
+ *    - through interface
+ *  - Typing object methods in an interface
+ *    - key: () => type;
+ *    - key() : type;
+ *  - Optional methods in the interface
+ */
+
+// const addExpression = function (x, y) {
+//   return x + y;
+// };
+
+export {};
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,5 @@
 // import "./02-arrays";
 // import "./03-types";
 // import "./04-interfaces";
-import "./05-enums";
+// import "./05-enums";
+import "./06-functions";
